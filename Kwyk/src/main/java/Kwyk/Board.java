e489626c10410408cafe0af7ab2ee13fbcc6a124
RABEHANTA Line <nayslen@yahoo.fr>
2021-02-09 18:43:00 +0300

    [test2] constructeur Board

--- Kwyk/src/main/java/Kwyk/Board.java
+++ Kwyk/src/main/java/Kwyk/Board.java
@@ -5,6 +5,20 @@ import java.awt.Color;
 public class Board {
     Pixel[][]p;
     
+    Board(int[][]patron) { // on genere directement le patron du dessin a faire : sous-tableaux de la forme {ligne/ordonnee, col/abscisse, code RVB de la couleur}
+    	this.p=new Pixel[400][400];
+    	int i=0; // curseur pour parcourir chaque sous tableau du patron, donc les coordonnees doivent etre mises dans l ordre (e.g (0,0) < (0,1) < (1,0))
+    	for(int row=0; row<p.length; row++) {
+    		for(int col=0; col<p[0].length; col++) {
+    			if (i<patron.length && row==patron[i][0] && col==patron[i][1]) { // si l on est au niveau d une case a peindre
+    				this.p[row][col]=new Pixel(new Color(patron[i][2]), true);
+    				i++;
+    			}
+    			else this.p[row][col]=new Pixel();
+    		}
+    	}
+    }
+    
     class Pixel{//class interne
         Color color;//couleur du pixel
         boolean toFill;//true si on doit repasser dessus
@@ -24,4 +38,5 @@ public class Board {
             this.color=color;
         }
     }//fin classe interne Pixel
+	
 }
