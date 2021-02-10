d0a3b0319a2e3d354e45d11cdc186ecf9bc78119
DAI anna <shana01112000@gmail.com>
2021-02-10 14:41:41 +0100

    Deleted Kwyk/src/main/java/Kwyk/Board.java, Kwyk/src/main/java/Kwyk/Control.java, Kwyk/src/main/java/Kwyk/Main.java, Kwyk/src/main/java/Kwyk/Model.java, Kwyk/src/main/java/Kwyk/Player.java, Kwyk/src/main/java/Kwyk/View.java files

--- Kwyk/src/main/java/Kwyk/Main.java
+++
@@ -1,9 +0,0 @@
-package Kwyk;
-
-import java.awt.EventQueue;
-
-public class Main {
-    public static void main(String[]a){
-        EventQueue.invokeLater(()-> new Control(new View(null)));
-    }
-}
--- Kwyk/src/main/java/Kwyk/Player.java
+++
@@ -1,9 +0,0 @@
-package Kwyk;
-
-public class Player {
-    final String name;
-    
-    Player(String name){
-        this.name=name;
-    }
-}
