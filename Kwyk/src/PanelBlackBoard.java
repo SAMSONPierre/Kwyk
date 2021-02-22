6eb93469296495a13486dcf4df90458601ed32a0
DAI Anna <shana01112000@gmail.com>
2021-02-22 19:38:37 +0100

    Ajout de la grille de Pierre (+quelques changements de nom de variables)

--- Kwyk/src/PanelBlackBoard.java
+++ Kwyk/src/PanelBlackBoard.java
@@ -2,24 +2,35 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class PanelBlackBoard extends JPanel{
-    boolean voirGrille=true;
+    boolean gridApparent=true;//par defaut, on voit la grille
     
     PanelBlackBoard(int heightButt){
         this.setBounds(20, 20+heightButt, 400, 400);//marge=20 à gauche, 20+bauteur d un bouton en haut, taille 400*400
         this.setBackground(Color.BLACK);//fond noir
+        setButton();
+    }
+    
+    void setButton(){
+        JButton seeGrid=new JButton("See grid");
+        seeGrid.addActionListener((event)->{
+            gridApparent=!gridApparent;
+            this.repaint();
+        });
+        this.add(seeGrid);
         
+        //autres boutons
     }
     
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
-        if(voirGrille) {
-            paintGrid(g);
-        }
+        if(gridApparent) paintGrid(g);
         
+        //test
         g2.setStroke(new BasicStroke(5));
         g.setColor(Color.red.darker());
         g.drawLine(100, 200, 100, 300);
@@ -30,7 +41,6 @@ public class PanelBlackBoard extends JPanel{
         g.setColor(Color.red);
         g.drawLine(100, 200, 100, 300);
         g.drawLine(100, 300, 200, 300);
-        
     }
     
     void paintGrid(Graphics g) {
