f469b000d137eabc33f0358b66a6b9eb10a5bb8a
DAI Anna <shana01112000@gmail.com>
2021-03-03 01:15:33 +0100

    Changement attributs des vecteurs --> calcul des positions à faire

--- Kwyk/src/Vector.java
+++ Kwyk/src/Vector.java
@@ -1,19 +1,17 @@
 import java.awt.Color;
 
 class Vector{
-    final int x, y;//coordonees de depart
-    final double distance;
+    final int x1, y1;//coordonees de depart
     final Color color;//couleur du trait
     
-    Vector(int x, int y, double distance, Color color){
-        this.x=x;
-        this.y=y;
-        this.distance=distance;
+    Vector(int x1, int y1, Color color){
+        this.x1=x1;
+        this.y1=y1;
         this.color=color;
     }
     
     Vector(){//pour initialiser un vecteur temporaire, et creer des vecteurs internes
-        this(0, 0, 0, Color.BLACK);
+        this(0, 0, Color.BLACK);
     }
     
     boolean sameVector(Vector vector2){//jamais utilise normalement (override)
@@ -26,22 +24,20 @@ class Vector{
     *****************/
     
     class VectorLine extends Vector{//trait droit
-        final int angle;//sur 360 degres (0° si le pinceau est comme ca "->", et ordre trigo)
-
-        VectorLine(int x, int y, int angle, double distance, Color color){
-            super(x, y, distance, color);
-            this.angle=angle;
+        final int x2, y2;//coordonees d arrivee
+        
+        VectorLine(int x1, int y1, int x2, int y2, Color color){
+            super(x1, y1, color);
+            this.x2=x2;
+            this.y2=y2;
         }
 
         boolean sameVector(Vector vector2){//verifier si deux vecteurs font le meme trait
-            if(vector2.getClass()==VectorLine.class && this.distance==vector2.distance && this.color==vector2.color){
-                if(this.angle%180==((VectorLine)vector2).angle%180){//angle oppose=angle+/-180
-                    if(this.x==vector2.x && this.y==vector2.y) return true;//meme vecteur
-                    //si vector2 est oppose a this, alors (vector2.x, vector2.y) est le point d arrivee, donc :
-                    double d=Math.sqrt(((double)(this.x-vector2.x)*(this.x-vector2.x)+(this.y-vector2.y)*(this.y-vector2.y)));
-                    return d==this.distance;//vecteur oppose (true) ou different (false)
-                }
-                return false;
+            if(vector2 instanceof VectorLine){
+                VectorLine v2=(VectorLine)vector2;
+                boolean same=(x1==v2.x1 && y1==v2.y1 && x2==v2.x2 && y2==v2.y2);
+                boolean reverse=(x1==v2.x2 && y1==v2.y2 && x2==v2.x1 && y2==v2.y1);
+                return (same || reverse) && color.equals(v2.color);
             }
             return false;
         }
@@ -50,15 +46,26 @@ class Vector{
     
     //--------a faire--------
     class VectorArc extends Vector{//arc
-        //attributs en plus ?
+        final int width, height, startAngle, scanAngle;
         
         //constructeur a modifier
-        VectorArc(int x, int y, double distance, Color color){
-            super(x, y, distance, color);
+        VectorArc(int x1, int y1, int width, int height, int startA, int scanA, Color color){
+            super(x1, y1, color);
+            this.width=width;
+            this.height=height;
+            this.startAngle=startA;
+            this.scanAngle=scanA;
         }
         
         boolean sameVector(Vector vector2){
-            
+            if(vector2 instanceof VectorArc){
+                VectorArc v2=(VectorArc)vector2;
+                boolean same=(x1==v2.x1 && y1==v2.y1 && startAngle==v2.startAngle);
+                boolean reverse=(startAngle==(v2.startAngle+v2.scanAngle)%360);
+                return (same || reverse) && width==v2.width && height==v2.height
+                    && scanAngle==v2.scanAngle && color.equals(v2.color);
+            }
+            //----------a faire----------
             return false;
         }
     }
