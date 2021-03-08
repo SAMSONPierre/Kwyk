2bef034626ef12bac179c773ae06e2fd8fea2173
DAI Anna <shana01112000@gmail.com>
2021-03-08 18:20:50 +0100

    Suppression des classes internes CustomJButton des sommaires

--- Kwyk/src/ViewSummaryChallenge.java
+++ Kwyk/src/ViewSummaryChallenge.java
@@ -1,12 +1,8 @@
-import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.FontMetrics;
-import java.awt.Graphics;
 import java.awt.Image;
 import java.io.File;
 import javax.imageio.ImageIO;
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class ViewSummaryChallenge extends ViewGame{//sommaire des defis
@@ -42,28 +38,5 @@ public class ViewSummaryChallenge extends ViewGame{//sommaire des defis
         File file=new File(path);
         File[] files=file.listFiles();
         return files;
-    }
-    
-    
-    class CustomJButton extends JButton{
-        private String text;
-        private Image image;
-        
-        CustomJButton(String text, Image image){
-            this.text=text;
-            this.image=image;
-        }
-        
-        protected void paintComponent(Graphics g){
-            super.paintComponent(g);
-            if(image==null) return;
-            g.drawImage(image, 0, 0, getWidth(), getHeight(), this);
-            if(text!=null){
-                g.setColor(Color.WHITE);
-                FontMetrics metric=g.getFontMetrics();
-                int width=metric.stringWidth(text);
-                g.drawString(text, (getWidth()-width)/2, (getHeight()+70)/2);
-            }
-        }
     }
 }
