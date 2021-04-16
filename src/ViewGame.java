027aabdd82d2ae9820313101a4a4ef82033f6c3f
SSailly <steven.sailly@gmail.com>
2021-04-16 18:50:34 +0200

    Ajout de la barre de progression
    Décrit la proportion de niveaux réussis par le joueur

--- src/ViewGame.java
+++ src/ViewGame.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GraphicsEnvironment;
@@ -7,6 +8,7 @@ import java.io.IOException;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 
 public class ViewGame extends View{//a une barre de controle superieur en plus
     private JButton training, challenge, create, logout;//boutons pour acceder aux autres pages
@@ -50,7 +52,7 @@ public class ViewGame extends View{//a une barre de controle superieur en plus
         topBar.setBounds(0, 0, width, create.getPreferredSize().height);//hauteur d un bouton
         this.add(topBar);
         
-        //pannel de gauche
+        //panel de gauche
         JPanel left=new JPanel();//training, challenge, create
         left.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
         left.add(training);
@@ -61,12 +63,36 @@ public class ViewGame extends View{//a une barre de controle superieur en plus
         //panel de droite
         JPanel right=new JPanel();//username, progression, bouton logout
         right.setLayout(new FlowLayout(FlowLayout.RIGHT, 0, 0));
-        right.add(new JLabel(super.getModel().getPlayer().username));
+        
+        right.add(new JLabel(super.getModel().getPlayer().username));//username
         right.add(new JLabel("     "));//separation entre username et barre de progression
-        /* ... barre de progression a ajouter plus tard */
+        right.add(new JLabel("  Total progress  "));
+        right.add(createProgressBar(this.getModel().getPlayer()));//barre de progression
         right.add(new JLabel("     "));//separation entre barre de progression et bouton
         right.add(logout);
         topBar.add(right);
+    }
+    
+    private JProgressBar createProgressBar(Player player) {
+    	JProgressBar res=new JProgressBar();
+    	boolean[][] playerBool=player.currentLevel;
+    	int succeded=nbSucceded(playerBool);
+    	int nbLvl=playerBool[0].length+playerBool[1].length;
+    	res.setStringPainted(true);
+    	res.setForeground(Color.DARK_GRAY);
+    	res.setValue(100*succeded/nbLvl);
+    	return res;
+    }
+    
+    private int nbSucceded(boolean[][] playerBool) {
+    	int res=0;
+    	for(boolean b:playerBool[0]) {
+    		if(b) res++;
+    	}
+    	for(boolean b:playerBool[1]) {
+    		if(b) res++;
+    	}
+    	return res-2;//niveau reussi si le suivant est deverouille
     }
     
     int getButtonHeight(){
