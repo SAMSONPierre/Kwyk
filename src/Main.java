94b485dad4dd8d14645212c3363f97711eca21b3
DAI anna <shana01112000@gmail.com>
2021-04-17 21:47:18 +0200

    Initialisation des répertoires avant de commencer le jeu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,23 @@
 import java.awt.EventQueue;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[]a){
-        EventQueue.invokeLater(()-> new Control(new ViewLogin()));
+        try{
+            String[] path=new String[15];//repertoires a initialiser avant de pouvoir jouer
+            String[] tmp={"training/1_Tutorial", "training/2_For", "training/3_If", 
+                "training/4_While", "training/5_Variable", "training/6_Function", "challenge"};
+            for(int i=0; i<7; i++){
+                path[2*i]="levels/"+tmp[i];
+                path[2*i+1]="preview/"+tmp[i];
+            }
+            path[14]="players";
+            for(int i=0; i<path.length; i++) Files.createDirectories(Paths.get(path[i]));
+            EventQueue.invokeLater(()-> new Control(new ViewLogin()));
+        }
+        catch(Exception e){
+            System.out.println("Failed to create directories.");
+        }
     }
 }
